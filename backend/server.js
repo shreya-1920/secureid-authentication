1472e32888b577833b06b1a37bcab2d0271faf68
shreya-1920 <shreyajain1902s@gmail.com>
2026-08-27 21:12:20 +0530

    allow vercel frontend

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,8 @@ app.use(express.json());
 app.use(cors({
     origin: [
         "http://localhost:5500",
-        "http://127.0.0.1:5500"
+        "http://127.0.0.1:5500",
+        "https://secureid-authentication-phi.vercel.app"
     ],
     credentials: true
 }));
